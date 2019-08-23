08f1ec237ae8da981a06f6a843b401116162e36d
JacoDTDev <jacodtdev@gmail.com>
2019-08-23 13:59:15 +0200

    56.1 Component State and Signup Form

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,10 +2,24 @@ import React from 'react';
 import {Link} from "react-router";
 
 export default class Signup extends React.Component{
+    constructor(props){
+        super(props);//because we are over-riding the super-constructor.
+        this.state ={
+            count:0
+        }
+    }
+    increment(){
+        //get old count value and plus 1
+        this.setState({
+            count: this.state.count+1
+        })
+    }
     render() {
         return <div>
             <h1>Signup for Short Link</h1>
-            <p>Signup form here</p>
+            <p>{this.state.count}</p>
+            <p><button onClick={this.increment.bind(this)}>+1</button>
+            <button onClick={()=>this.setState({count: this.state.count-1}).bind(this)}>-1</button></p>
             <p><Link to="/">Go to Login.</Link></p>
         </div>
     }
