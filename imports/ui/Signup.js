781815e04b41ff6b8bd85f37410081a85fd9fb0e
JacoDTDev <jacodtdev@gmail.com>
2019-08-23 14:28:11 +0200

    56.2 Component State and Signup Form

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -5,21 +5,27 @@ export default class Signup extends React.Component{
     constructor(props){
         super(props);//because we are over-riding the super-constructor.
         this.state ={
-            count:0
+            error:''
         }
     }
-    increment(){
-        //get old count value and plus 1
+    onSubmit(e){
+        e.preventDefault(); //e is for error, this prevents the page from refreshing
         this.setState({
-            count: this.state.count+1
-        })
+            error:'Something went wrong?' //set the error message equal to
+        });
     }
     render() {
         return <div>
             <h1>Signup for Short Link</h1>
+
+            {this.state.error? <p>{this.state.error}</p>:undefined}
+
             <p>{this.state.count}</p>
-            <p><button onClick={this.increment.bind(this)}>+1</button>
-            <button onClick={()=>this.setState({count: this.state.count-1}).bind(this)}>-1</button></p>
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <input type='email' name='email' placeholder='Email'/>
+                <input type='password' name='password' placeholder='Password'/>
+                <button>Create Account</button>
+            </form>
             <p><Link to="/">Go to Login.</Link></p>
         </div>
     }
