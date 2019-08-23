18c289fde93d9d5e5cf257758c211a8dcd6a321a
JacoDTDev <jacodtdev@gmail.com>
2019-08-23 12:04:40 +0200

    53. Setting up React-Routing

---
+++ imports/ui/Signup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Signup extends React.Component{
+    render() {
+        return <div>
+            <h1>Signup for Short Link</h1>
+        </div>
+    }
+}
